bd521d3bcc867781351a8fef4a9f90245885b503
Uncurlhalo <jmelton116@gmail.com>
2024-09-06 13:11:01 -0400

    Add a clock and delta to track ticks in game

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     # init and set resolution
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # setup a clock and delta variable
+    clock = pygame.time.Clock()
+    dt = 0
 
+    # Event loop
     while True:
         # Check for QUIT events
         for event in pygame.event.get():
@@ -20,6 +21,10 @@ def main():
         # fill display with black and update
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        # tick the clock and save to delta 
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
