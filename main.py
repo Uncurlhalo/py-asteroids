da39a4f0690a064ad4f241892a785aa69e251fc9
Uncurlhalo <jmelton116@gmail.com>
2024-09-06 13:05:30 -0400

    Add main event loop, constants, and basic quit handling

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # init and set resolution
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # Check for QUIT events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        # fill display with black and update
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
